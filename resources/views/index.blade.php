1b5aa54bc94cb2d91c65d5e7bd710b33632863a8
utsab420575 <utsab@duet.ac.bd>
2025-07-21 07:50:46 +0600

    dashaboard

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,59 +18,96 @@
         </header>
         <!-- start: page -->
         <div class="row">
-            <div class="col-md-6 col-lg-6">
-                <div class="row mb-3">
-                    <div class="col-md-6 col-xl-6">
-                        <section class="card card-featured-left card-featured-primary mb-3">
-                            <div class="card-body">
-                                <div class="widget-summary d-flex align-items-center">
-                                    <!-- Icon Column -->
-                                    <div class="widget-summary-col widget-summary-col-icon">
-                                        <div class="summary-icon bg-primary">
-                                            <i class="fa-solid fa-person-chalkboard"></i>
-                                        </div>
-                                    </div>
+            <!-- Completed Card -->
+            <div class="col-sm-12 col-md-6 col-xl-4 ">
+                <section class="card card-featured-left card-featured-success mb-3">
+                    <div class="card-body">
+                        <div class="widget-summary d-flex align-items-center">
+                            <!-- Icon Column -->
+                            <div class="widget-summary-col widget-summary-col-icon">
+                                <div class="summary-icon bg-success">
+                                    <i class="fas fa-check-circle"></i>
+                                </div>
+                            </div>
 
-                                    <!-- Text Column -->
-                                    <div class="widget-summary-col flex-grow-1 d-flex align-items-center" style="padding-left: 100px;">
-                                        <div class="summary">
-                                            <h4 class="title">Architecture Department Teachers</h4>
-                                            <div class="info">
-                                                <strong class="amount">10</strong>
-                                            </div>
-                                        </div>
+                            <!-- Text Column -->
+                            <div class="widget-summary-col flex-grow-1 d-flex align-items-center" style="padding-left: 100px;">
+                                <div class="summary text-center">
+                                    <h4 class="title mb-1 fs-4">Completed</h4> {{-- Bootstrap fs-4 for larger title --}}
+                                    <div class="info">
+                                        <strong class="amount fs-3">0 / {{ $imageCount }}</strong> {{-- Larger count --}}
                                     </div>
                                 </div>
                             </div>
-                        </section>
+                        </div>
                     </div>
+                </section>
+            </div>
 
-                    <div class="col-md-6 col-xl-6">
-                        <section class="card card-featured-left card-featured-secondary">
-                            <div class="card-body">
-                                <div class="widget-summary d-flex">
-                                    <div class="widget-summary-col widget-summary-col-icon">
-                                        <div class="summary-icon bg-secondary">
-                                            <i class="fas fa-user-tie"></i>
-                                        </div>
-                                    </div>
-                                    <div class="widget-summary-col d-flex align-items-center justify-content-center">
-                                        <div class="summary">
-                                            <h4 class="title">Employee Count </h4>
-                                            <div class="info">
-                                                <strong class="amount">10</strong>
-                                            </div>
-                                        </div>
+            <!-- Pending Card -->
+            <div class="col-sm-12 col-md-6 col-xl-4">
+                <section class="card card-featured-left card-featured-warning mb-3">
+                    <div class="card-body">
+                        <div class="widget-summary d-flex align-items-center">
+                            <!-- Icon Column -->
+                            <div class="widget-summary-col widget-summary-col-icon">
+                                <div class="summary-icon bg-warning">
+                                    <i class="fas fa-hourglass-half"></i>
+                                </div>
+                            </div>
 
+                            <!-- Text Column -->
+                            <div class="widget-summary-col flex-grow-1 d-flex align-items-center" style="padding-left: 100px;">
+                                <div class="summary text-center">
+                                    <h4 class="title mb-1 fs-4">Pending</h4> {{-- Bootstrap fs-4 for larger title --}}
+                                    <div class="info">
+                                        <strong class="amount fs-3">0 / {{ $imageCount }}</strong> {{-- Larger count --}}
                                     </div>
                                 </div>
                             </div>
-                        </section>
+                        </div>
                     </div>
-                </div>
+                </section>
+            </div>
 
+            <!-- Dispute Card -->
+            <div class="col-sm-12 col-md-6 col-xl-4">
+                <section class="card card-featured-left card-featured-danger mb-3">
+                    <div class="card-body">
+                        <div class="widget-summary d-flex align-items-center">
+                            <!-- Icon Column -->
+                            <div class="widget-summary-col widget-summary-col-icon">
+                                <div class="summary-icon bg-danger">
+                                    <i class="fas fa-gavel"></i> {{-- Larger icon --}}
+                                </div>
+                            </div>
+
+                            <!-- Text Column -->
+                            <div class="widget-summary-col flex-grow-1 d-flex align-items-center" style="padding-left: 100px;">
+                                <div class="summary text-center">
+                                    <h4 class="title mb-1 fs-4">Dispute</h4> {{-- Bootstrap fs-4 for larger title --}}
+                                    <div class="info">
+                                        <strong class="amount fs-3">0 / {{ $imageCount }}</strong> {{-- Larger count --}}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4 text-center">
+                <button class="btn btn-primary px-5 py-4 fs-4">
+                    Start Execution <i class="fas fa-arrow-right ms-2"></i>
+                </button>
+            </div>
+            <div class="col-md-4"></div>
+
+        </div>
+
         <!-- end: page -->
     </section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,23 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('index');
+    $folderPath = 'C:\Users\User\Desktop\DUET\First_Part';
+
+    if (!File::exists($folderPath)) {
+        $imageCount = 0;
+    } else {
+        $files = File::files($folderPath);
+
+        $imageCount = collect($files)->filter(function ($file) {
+            return in_array(strtolower($file->getExtension()), ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp']);
+        })->count();
+    }
+    return view('index', [
+        'imageCount' => $imageCount,
+        'completedPercent' => 10, // or calculated value
+    ]);
+
+
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
